fa6cc2dd9b8cab361a6ca209c065a732d6f59ae7
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-10 21:16:47 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,17 @@
 # Write your methods here
+def loop_message_five_times(message)
+  5.times {puts message}
+end 
+
+def loop_message_n_times(message, number)
+  number.times {puts message}
+end 
+
+def output_array(array)
+  puts array
+end
+
+def return_string_array(array)
+  #each destructives the array, collect creates new one 
+  array.collect {|i| i.to_s} 
+end 
